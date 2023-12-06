ee7040682689ee265feb63241fc44bb9744a4c30
Mihle <git@mihle.dev>
2023-12-06 13:39:25 +0100

    Day 6, Part 1 and 2

---
+++ src/day_6/Part_2.java
@@ -0,0 +1,43 @@
+package day_6;
+
+import java.util.ArrayList;
+
+import static common.ImportFile.fileToArray;
+
+public class Part_2 {
+    public static void main(String[] args) {
+
+//        Import file as array per line
+        ArrayList<String> inputLines = fileToArray("src\\day_6\\input.txt");
+
+//        Removes everything except the numbers and converts to int/Long
+        for (int i = 0; i < inputLines.size(); i++) {
+            inputLines.set(i, inputLines.get(i).replaceFirst(".+:", ""));
+            inputLines.set(i, inputLines.get(i).trim().replaceAll(" +", ""));
+        }
+
+        int time = Integer.parseInt(inputLines.get(0));
+        long distance = Long.parseLong(inputLines.get(1));
+
+        // TODO: 06.12.2023 Use math instead of loops 
+//       Calculates max and minimum time
+        int maxTime = 0;
+        int minTime = 0;
+        for (int j = time; j > 0; j--) {
+            if ((long) (time - j) * j > distance) {
+                maxTime = j;
+                break;
+            }
+        }
+
+        for (int j = 0; j < time; j++) {
+            if ((long) (time - j) * j > distance) {
+                minTime = j;
+                break;
+            }
+        }
+
+//        Calculates and prints sum
+        System.out.println("Sum: " + (maxTime - minTime + 1));
+    }
+}
