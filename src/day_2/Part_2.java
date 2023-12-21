412ebdd57b89078910fff723c24e7708eb6acb05
Mihle <git@mihle.dev>
2023-12-21 13:18:30 +0100

    Improved Day 2 part 1, and completed Day 2 part 2

--- src/day_2/Part_2.java
+++ src/day_2/Part_2.java
@@ -3,74 +3,66 @@ package day_2;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import static common.ImportFile.fileToArray;
 
-public class Part_1 {
+public class Part_2 {
     public static void main(String[] args) {
 
 //        Import file as array
         ArrayList<String> inputLines = fileToArray("src\\day_2\\input.txt");
 
-//        Max number to look for
-        int maxRed = 12;
-        int maxGreen = 13;
-        int maxBlue = 14;
-
 //        Pattern for what to find
         Pattern colorPattern = Pattern.compile("red|green|blue");
         Pattern numberPattern = Pattern.compile("\\d+");
 
-//        Sum for Game ID:
         int sum = 0;
 
-        for (int i = 0; i < inputLines.size(); i++) {
-            String[] colorCubeArray = inputLines.get(i).split(": ")[1]
-                                                       .split("[;,]");
+        for (String inputLine : inputLines) {
+            String[] colorCubeArray = inputLine.split(": ")[1]
+                                               .split("[;,]");
 
-//            Finds games with too many of a coloured cube
+//                Highest number of each colour found
+                int redHigh = 0;
+                int greenHigh = 0;
+                int blueHigh = 0;
+
+//                Finds highest number of each colour found
             for (String cube : colorCubeArray) {
                 Matcher matcher = colorPattern.matcher(cube);
                 matcher.find();
 
 //                Red cubes
-                if (matcher.group().equals("red")){
+                if (matcher.group().equals("red")) {
                     matcher = numberPattern.matcher(cube);
                     matcher.find();
-                    if (Integer.parseInt(matcher.group()) > maxRed) {
-                        sum += i+1;
-                        break;
+                    if (Integer.parseInt(matcher.group()) > redHigh) {
+                        redHigh = Integer.parseInt(matcher.group());
                     }
 
 //                    Green Cubes
                 } else if (matcher.group().equals("green")) {
                     matcher = numberPattern.matcher(cube);
                     matcher.find();
-                    if (Integer.parseInt(matcher.group()) > maxGreen) {
-                        sum += i+1;
-                        break;
+                    if (Integer.parseInt(matcher.group()) > greenHigh) {
+                        greenHigh = Integer.parseInt(matcher.group());
                     }
 
 //                    Blue Cubes
                 } else {
                     matcher = numberPattern.matcher(cube);
                     matcher.find();
-                     if (Integer.parseInt(matcher.group()) > maxBlue) {
-                         sum += i+1;
-                         break;
-                   }
+                    if (Integer.parseInt(matcher.group()) > blueHigh) {
+                        blueHigh = Integer.parseInt(matcher.group());
+                    }
                 }
             }
-        }
 
-//        Caluclates sum, inverts it
-        int totalSum = 0;
-        for (int i = 1; i <=100 ; i++) {
-            totalSum += i;
+//            Calculating sum
+            sum += redHigh * greenHigh * blueHigh;
         }
 
 //        Prints sum
-        System.out.println("Max sum: " + totalSum);
-        System.out.println("Calculated sum: " + sum);
-        System.out.println("Actual Sum: " + (totalSum-sum));
+        System.out.println("Sum: " + sum);
     }
 }
