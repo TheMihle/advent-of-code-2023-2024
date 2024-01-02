46517496f94234cdadfd81630c7b51eaf1079909
Mihle <git@mihle.dev>
2024-01-02 22:02:54 +0100

    Switch from Arraylist to array to make code simpler Day 11

--- src/day_11/Part_1.java
+++ src/day_11/Part_1.java
@@ -1,25 +1,15 @@
 package day_11;
 
-import java.awt.*;
+import java.awt.Point;
 import java.util.ArrayList;
-import java.util.Arrays;
 
-import static common.ImportFile.fileToArray;
+import static common.ImportFile.fileTo2DArray;
 
 public class Part_1 {
     public static void main(String[] args) {
 
-//        Import file as array per line
-        ArrayList<String> inputLines = fileToArray("src\\day_11\\input.txt");
-
-//        Create an 2D array
-        ArrayList<ArrayList<String>> spaceCoordinates= new ArrayList<>();
-
-        for (String inputLine : inputLines) {
-            String[] horizontal = inputLine.split("");
-            ArrayList<String> tempList = new ArrayList<>(Arrays.asList(horizontal));
-            spaceCoordinates.add(tempList);
-        }
+//        Import file as 2D array
+        String[][] spaceCoordinates= fileTo2DArray("src\\day_11\\input.txt");
 
         long sum = 0;
 
@@ -28,10 +18,10 @@ public class Part_1 {
         ArrayList<Integer> expSpaceY = new ArrayList<>();
 
 //        Finds where space is expanded horizontally
-        for (int x = 0; x < spaceCoordinates.get(0).size(); x++) {
+        for (int x = 0; x < spaceCoordinates[0].length; x++) {
             boolean galaxies = false;
-            for (ArrayList<String> rowArray : spaceCoordinates) {
-                if (rowArray.get(x).equals("#")) {
+            for (String[] rowArray : spaceCoordinates) {
+                if (rowArray[x].equals("#")) {
                     galaxies = true;
                     break;
                 }
@@ -43,10 +33,10 @@ public class Part_1 {
         }
 
 //        Finds where space is expanded vertically
-        for (int y = 0; y < spaceCoordinates.size(); y++) {
+        for (int y = 0; y < spaceCoordinates.length; y++) {
             boolean galaxies = false;
-            for (int x = 0; x < spaceCoordinates.get(y).size(); x++) {
-                if (spaceCoordinates.get(y).get(x).equals("#")) {
+            for (int x = 0; x < spaceCoordinates[y].length; x++) {
+                if (spaceCoordinates[y][x].equals("#")) {
                     galaxies = true;
                     break;
                 }
@@ -64,9 +54,9 @@ public class Part_1 {
 //        Creates array with coordinates of the galaxies
         ArrayList<Point> galaxyCoordinates = new ArrayList<>();
 
-        for (int y = 0; y < spaceCoordinates.size(); y++) {
-            for (int x = 0; x < spaceCoordinates.get(y).size(); x++) {
-                if (spaceCoordinates.get(y).get(x).equals("#")){
+        for (int y = 0; y < spaceCoordinates.length; y++) {
+            for (int x = 0; x < spaceCoordinates[y].length; x++) {
+                if (spaceCoordinates[y][x].equals("#")){
                     galaxyCoordinates.add(new Point(x,y));
                 }
             }
