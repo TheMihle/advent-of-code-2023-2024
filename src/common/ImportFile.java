bb32dac01386f466b1c87622c42250aa6168ad35
Mihle <git@mihle.dev>
2024-12-18 22:31:15 +0100

    Improved importFile class methods with try with resources and StringBuilder

--- src/common/ImportFile.java
+++ src/common/ImportFile.java
@@ -6,31 +6,25 @@ import java.util.Scanner;
 
 public class ImportFile {
 
-    //    Converts a file in to an ArrayList, one line per element
+//    Converts a file in to an ArrayList, one line per element
     public static ArrayList<String> fileToArray(String path) {
 
-//        Creates scanner and array
-        Scanner scanner = null;
         ArrayList<String> inputLines = new ArrayList<>();
 
-//        Loads file with text to scanner
+//        Loads file with text to scanner to load it in to the array
         File inputFile = new File(path);
-        try {
-            scanner = new Scanner(inputFile);
+        try (Scanner scanner = new Scanner(inputFile)){
+            while (scanner.hasNextLine()) {
+                inputLines.add(scanner.nextLine());
+            }
         } catch (Exception e) {
-            System.out.println("File not found: " + e);
+            System.out.println("File not found or failed to load: " + e);
         }
 
-//        Puts lines in file in to array and returns it
-        while (scanner.hasNextLine()) {
-            inputLines.add(scanner.nextLine());
-        }
         return inputLines;
     }
-    public static String[][] fileTo2DArray(String path){
-        return fileTo2DArray(path, "");
-    }
 
+//    Converts file to 2D array, with a specified split regex
     public static String[][] fileTo2DArray(String path, String regex) {
 
 //        Import file as array per line
@@ -46,35 +40,35 @@ public class ImportFile {
         return coordinate2DArray;
     }
 
-//    Converts a file to a String, all lines added together
-    public static String fileToString(String path) {
-
-//        Creates scanner and string
-        Scanner scanner = null;
-        String inputString = "";
-
-//        Loads file with text to scanner
-        File inputFile = new File(path);
-        try {
-            scanner = new Scanner(inputFile);
-        } catch (Exception e) {
-            System.out.println("File not found: " + e);
-        }
-
-//        Puts lines in file in to array and returns it
-        while (scanner.hasNextLine()) {
-            inputString += scanner.nextLine();
-        }
-
-        return inputString;
+//    Converts file to 2D array, one symbol per element
+    public static String[][] fileTo2DArray(String path){
+        return fileTo2DArray(path, "");
     }
 
-//    Converts file to a string, then splits to array
+//    Converts a file to a String, all lines added together
+    public static String fileToString(String path) {
+        StringBuilder inputString = new StringBuilder();
+
+//        Loads file with text to scanner and convert to one string
+        File inputFile = new File(path);
+        try (Scanner scanner = new Scanner(inputFile)){
+            while (scanner.hasNextLine()) {
+                inputString.append(scanner.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("File not found or failed to load: " + e);
+        }
+
+        return inputString.toString();
+    }
+
+//    Converts file to a string, then splits to array based in input regex
     public static String[] fileToSplittedString(String path, String regex) {
         String inputString = fileToString(path);
         return inputString.split(regex);
     }
 
+//    Converts file to string, then splits to array with one symbol per element
     public static String[] fileToSplittedString(String path) {
         return fileToSplittedString(path, "");
     }
