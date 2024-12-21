ec9c9d909d7d3f3c53cf2d350ae4369a7e7e0ccc
Mihle <git@mihle.dev>
2024-12-21 23:07:33 +0100

    Day 19, Part 2 with memory

---
+++ src/year_2024/day_19/Part_2.java
@@ -0,0 +1,54 @@
+package year_2024.day_19;
+
+import java.util.*;
+
+import static common.ImportFile.fileToArray;
+
+public class Part_2 {
+    public static void main(String[] args) {
+
+//        Import file as array
+        ArrayList<String> designs = fileToArray("src\\year_2024\\day_19\\input.txt");
+
+//        Extract patterns, add to HashSet
+        String[] patterns = designs.getFirst().split(", " );
+        designs.removeFirst();
+        designs.removeFirst();
+        HashSet<String> hashPatterns = new HashSet<>(Arrays.asList(patterns));
+
+        int longestPattern = 0;
+        for (String pattern : patterns) {
+            if (pattern.length() > longestPattern) longestPattern = pattern.length();
+        }
+
+//        Counts possible combinations with provided patterns with memory
+        long possibleCombinations = 0;
+        HashMap<String, Long> memory = new HashMap<>();
+
+        for (String design : designs) {
+            possibleCombinations += findMatch(design, hashPatterns, memory, longestPattern);
+        }
+
+        System.out.println("Day 19, Part 2, Number of possible design combinations: " + possibleCombinations);
+    }
+
+//    Recursive method that finds matches at the start of the design, calls itself recursively on the next part of the design.
+//    Returns number of possible combinations.
+    private static long findMatch(String subDesign, HashSet<String> patterns, HashMap<String, Long> memory, int maxLength) {
+        if (subDesign.isEmpty()) return 1;
+//        Skips if substring have already been calculated before, happens A LOT of times
+        if (memory.containsKey(subDesign)) return memory.get(subDesign);
+
+        long combinations = 0;
+        for (int i = 1; i <= maxLength; i++) {
+            if (i > subDesign.length()) continue;
+            if (patterns.contains(subDesign.substring(0, i))) {
+                combinations += findMatch(subDesign.substring(i), patterns, memory, maxLength);
+            }
+        }
+
+//        Save new calculations of number combinations
+        memory.put(subDesign, combinations);
+        return combinations;
+    }
+}
