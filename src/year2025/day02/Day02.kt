72294a1aff2562c82a8a2ce48f6d2290512f2527
TheMihle <141750209+s371426@users.noreply.github.com>
2025-12-08 22:06:31 +0100

    2025, Day 2, Part 2 Complete

--- src/year2025/day02/Day02.kt
+++ src/year2025/day02/Day02.kt
@@ -9,12 +9,17 @@ fun main() {
     // To 2D Long array.
     val ranges = input.asSequence().map { it -> it.split("-").map { it.toLong() }}.toList()
 
-    part1(ranges)
-    part2(ranges)
+    val sumPart1 = part1(ranges)
+    val sumPart2 = part2(ranges)
+
+    println("Day 2, Part 1, Sum of invalid IDs: $sumPart1" )
+    println("Day 2, Part 2, Sum of invalid IDs: $sumPart2" )
 }
 
-fun part1(ranges: List<List<Long>>) {
-    var result = 0L
+// Divides the number in two and checks if two parts is equal. Sums together if true.
+// Faster than part 2 regex solution
+fun part1(ranges: List<List<Long>>): Long {
+    var sumOfIds = 0L
     for (range in ranges) {
         for (number in range.first()..range.last()){
             val string = number.toString()
@@ -23,19 +28,25 @@ fun part1(ranges: List<List<Long>>) {
 
             if (split.size < 2) continue
             if (split.first() == split.last()) {
-                result += number
+                sumOfIds += number
+            }
+        }
+    }
+    return sumOfIds
+}
+
+// Uses regex to a check if a value is repetition of the same combination of digits 2+ times.
+// Sums together if true.
+fun part2(ranges: List<List<Long>>): Long {
+    var sumOfIds = 0L
+    for (range in ranges) {
+        for (number in range.first()..range.last()){
+            val string = number.toString()
+            if (string matches "(\\d+)\\1+".toRegex()) {
+                sumOfIds += number
             }
         }
     }
 
-    println("Day 2, Part 1, Sum of invalid IDs: $result" )
+    return sumOfIds
 }
-
-fun part2(ranges: List<List<Long>>) {
-
-}
-
-
-
-
-
